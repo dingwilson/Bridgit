5d370ad9b9cf9acec6f7f03f7b012bf68dae228d
Wilson Ding <shodai100@gmail.com>
2016-11-17 14:41:22 -0800

    Added Splash Page and Beginning work on Pairing

---
+++ Pods/Target Support Files/Pods-Bridgit/Pods-Bridgit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bridgit : NSObject
+@end
+@implementation PodsDummy_Pods_Bridgit
+@end
---
+++ Pods/Target Support Files/Pods-Bridgit/Pods-Bridgit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BridgitVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BridgitVersionString[];
+
